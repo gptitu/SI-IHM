74ec72c3a7c6ceba22913ed3d35636670584074e
gptitu <gptitu@gmail.com>
2017-06-23 09:49:44 +0300

    ajout imagedao

---
+++ model/Image.php
@@ -0,0 +1,29 @@
+<?php
+	class Achat{
+		
+		private $id = "";
+		private $jeu = null;
+		private $nom = "";
+		
+		public function __construct($i, $j, $n){
+			$this->id = $i;
+			$this->jeu = $j;
+			$this->nom = $n;
+		}
+		
+		public function getId(){
+			return $this->id;
+		}
+		public function getJeu(){
+			return $this->jeu;
+		}
+		public function getNom(){
+			return $this->nom;
+		}
+		
+		public function toString(){
+			return $this->id." : ".$this->jeu->getId()." : ".$this->nom."<br/>";
+		}
+		
+	}
+?>
---
+++ model/ImageDAO.php
@@ -0,0 +1,103 @@
+<?php
+	
+	include('Image.php');
+	/*include('Utilisateur.php');
+	include('UtilisateurDAO.php');
+	include('Jeu.php');
+	include('JeuDAO.php');*/
+	
+	class imageDAO{
+		
+		private $img;
+		private $result;
+		private $bdd = null;
+		
+		public function __construct($connexion){
+			$this->bdd = $connexion;
+		}	
+		
+		public function loadData($condition){
+			
+			$request = "SELECT Image.* from Image image";
+			if($condition != null){
+				$request = $request." ".$condition;
+			}
+			
+			$this->result = $this->bdd->query($request);
+			$this->result->setFetchMode(PDO::FETCH_OBJ);
+			
+			$jeudao = new JeuDAO($this->bdd);
+			$jeu =null;
+			$_image = null;
+			
+			while($_image = $this->result->fetch()){
+				$jeu = $jeudao->loadData("Where id = '" . $_image->jeu."'");
+				$img[] = new Image($_image->id, $jeu[0], $_image->nom);
+			} return $img;
+		}
+		
+		public function insertData($_image){
+			
+			if($_image instanceof Image){
+				
+					$req = "INSERT INTO image VALUES('".$_image->getId()."', '".$_image->getJeu()->getId()."', '".$_image->getNom()."')";
+					echo $req;
+					$n = $this->bdd->exec($req);
+					return $n;
+				
+			} else{ echo 'Erreur : cette variable n\'est pas un objet de type image ...'; }
+			
+		}
+		
+		public function deleteData($_image){
+			
+			if($_image instanceof Image){
+				
+				$n = $this->bdd->exec("DELETE FROM Image WHERE id='".$_image->getId()."'");
+				return $n;
+				
+			} else{ echo 'Erreur : cette variable n\'est pas un objet de type image ...'; }
+			
+		}
+		
+		public function updateData($_image){
+			
+			if($_image instanceof Image){
+				
+				$settings = "SET id='".$_image->getId()."', jeu='".$_image->getJeu()->getId()."', nom='".$_image->getNom()."'";
+				
+				$n = $this->bdd->exec("UPDATE image ".$settings." WHERE id='".$_image->getId()."'"); 
+				return $n;
+				
+			} else{ echo 'Erreur'; }
+			
+		}
+		
+		public function showAll(){
+			$images = $this->loadData(null);
+			$i = 0;
+			while($i < count($images)){
+					echo $images[$i]->toString();
+					$i++;
+			}
+		}
+		
+		public function nextId(){
+		
+			$id = "IM";
+			
+			$odao = new OtherDAO($this->bdd);
+			
+			$rs = $odao->loadData("SELECT nextval('seqImage')");
+			$nx = ""+$rs[0]->nextval;
+			
+			while(strlen($nx) < 3){
+				$nx = "0".$nx;
+			} $id = $id . $nx;
+			
+			return $id;
+		
+		}
+		
+	}
+?>
